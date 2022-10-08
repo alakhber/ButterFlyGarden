cf0b9c45877c86c78f55846a25ea1b34d752da1f
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2022-10-08 11:55:24 +0400

    added route for project build

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,22 +17,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class,'index'])->name('home');
-Route::get('/services', [HomeController::class,'services'])->name('services');
-Route::get('/service/{slug}', [HomeController::class,'service'])->name('service');
-Route::get('/about', [HomeController::class,'about'])->name('about');
-Route::get('/blogs', [HomeController::class,'blogs'])->name('blogs');
-Route::get('/contact', [HomeController::class,'contact'])->name('contact');
-Route::get('/blog/{slug}', [HomeController::class,'blog'])->name('blog');
-Route::get('/projects', [HomeController::class,'projects'])->name('projects');
-Route::get('/project/{slug}', [HomeController::class,'project'])->name('project');
-Route::get('/products/{slug?}', [HomeController::class,'products'])->name('products');
-Route::get('/product/{slug}', [HomeController::class,'product'])->name('product');
-Route::get('/page/{slug}', [HomeController::class,'page'])->name('page');
-Route::post('/subscription', [HomeController::class,'subscription'])->name('subscription');
-Route::post('/contact', [HomeController::class,'contactMail'])->name('contact-mail');
+Route::get('build', function () {
+    try {
+        Artisan::call('migrate:fresh');
+        Artisan::call('db:seed');
+        Artisan::call('storage:link');
+        dd('ok');
+    } catch (\Throwable $th) {
+        throw $th;
+    }
+});
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/services', [HomeController::class, 'services'])->name('services');
+Route::get('/service/{slug}', [HomeController::class, 'service'])->name('service');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/blogs', [HomeController::class, 'blogs'])->name('blogs');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/blog/{slug}', [HomeController::class, 'blog'])->name('blog');
+Route::get('/projects', [HomeController::class, 'projects'])->name('projects');
+Route::get('/project/{slug}', [HomeController::class, 'project'])->name('project');
+Route::get('/products/{slug?}', [HomeController::class, 'products'])->name('products');
+Route::get('/product/{slug}', [HomeController::class, 'product'])->name('product');
+Route::get('/page/{slug}', [HomeController::class, 'page'])->name('page');
+Route::post('/subscription', [HomeController::class, 'subscription'])->name('subscription');
+Route::post('/contact', [HomeController::class, 'contactMail'])->name('contact-mail');
 
 // Route::get('/products-by-category/{slug}', [HomeController::class,'productsByCategory'])->name('products-by-category');
 
 Auth::routes();
-
